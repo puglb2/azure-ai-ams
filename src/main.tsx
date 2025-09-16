e5efdf6eb17f3d5ab6a485628c1d0f39abf72084
puglb2 <puglb2@gmail.com>
2025-09-16 14:16:44 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,5 @@
-
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+const el = document.getElementById('ams-intake-chat')!
+createRoot(el).render(<App />)
